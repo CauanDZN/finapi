29998a1b0731586ae4cdc1fcc73fbd47cc846a3e
Cauan Victor <cauanvictor0325@gmail.com>
2022-08-13 17:22:34 -0300

    :id: Adding UUID

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid")
 
 const app = express();
 
+const customers = [];
+
 app.use(express.json());
 
-app.get("/coursers", (request, response) => {
-    return response.json({message: "Hello World!"});
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+    if(customerAlreadyExists){
+        return response.status(400).json({error: "Customer already exists."})
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return response.status(201).send()
+})
+
+app.get("/statement", (request, response) => {
+    const {cpf} = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer){
+        return response.status(400).json({error: "Customer not found."})
+    }
+
+    return response.json(customer.statement)
 })
 
 app.listen(3031);
